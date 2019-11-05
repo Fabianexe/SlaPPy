fa566caaa5bb4eecf96e600474042854d4956365
Fabian Gärtner <fabian@bioinf.uni-leipzig.de>
2019-11-05 20:48:21 +0100

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FIPY',
+    version='0.1',
+    packages=['fipy'],
+    url='',
+    license=' GNU GPLv3',
+    author='Fabian Gärtner',
+    author_email='fabianexe@gmail.com',
+    description='Fast5 In PrettY'
+)
